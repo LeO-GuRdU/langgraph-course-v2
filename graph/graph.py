66873d53e51a2545a71ba93b3da036b8727b0fab
leonardo-gurdulich <leonardo.gurdulich@halopowered.com>
2025-11-21 20:51:52 -0300

    Solucionado error con graph

--- graph/graph.py
+++ graph/graph.py
@@ -44,7 +44,7 @@ def grade_generation_grounded_in_documents_and_questions(state: GraphState) -> s
         return "not supported"
     
 def route_question(state: GraphState) -> str:
-    print("---ROUTING QUESTION---")
+    print("---ROUTE QUESTION---")
     question = state["question"]
     source: RouteQuery = question_router.invoke({"question": question})
     if source.datasource == WEBSEARCH:
@@ -63,23 +63,33 @@ workflow.add_node(WEBSEARCH, web_search)
 
 workflow.set_conditional_entry_point(
     route_question,
-    {WEBSEARCH: WEBSEARCH, RETRIEVE: RETRIEVE},
+    {
+        WEBSEARCH: WEBSEARCH,
+        RETRIEVE: RETRIEVE,
+    },
 )
-
-workflow.set_entry_point(RETRIEVE)
 workflow.add_edge(RETRIEVE, GRADE_DOCUMENTS)
 workflow.add_conditional_edges(
-    GRADE_DOCUMENTS, decide_to_generate, {WEBSEARCH: WEBSEARCH, GENERATE: GENERATE}
+    GRADE_DOCUMENTS,
+    decide_to_generate,
+    {
+        WEBSEARCH: WEBSEARCH,
+        GENERATE: GENERATE,
+    },
 )
+
 workflow.add_conditional_edges(
     GENERATE,
     grade_generation_grounded_in_documents_and_questions,
-    {"useful": END, "not useful": WEBSEARCH, "not supported": GENERATE},
+    {
+        "not supported": GENERATE,
+        "useful": END,
+        "not useful": WEBSEARCH,
+    },
 )
-
 workflow.add_edge(WEBSEARCH, GENERATE)
 workflow.add_edge(GENERATE, END)
 
 app = workflow.compile()
 
-app.get_graph().draw_mermaid_png(output_file_path="graph_output.png")
+app.get_graph().draw_mermaid_png(output_file_path="graph.png")
--- graph/nodes/web_search.py
+++ graph/nodes/web_search.py
@@ -9,10 +9,13 @@ load_dotenv()
 web_search_tool = TavilySearch(max_results=3)
 
 def web_search(state: GraphState) -> Dict[str, Any]:
-    print("--- Performing web search for the question ---")
+    print("---WEB SEARCH---")
     question = state["question"]
-    documents = state["documents"]
-
+    if "documents" in state: # if the route to web search in first time then give error
+        documents = state["documents"]
+    else:
+        documents = None
+        
     tavily_results = web_search_tool.invoke({"query": question})["results"]
     joined_tavily_result = "\n".join(
         [tavily_result["content"] for tavily_result in tavily_results]
@@ -24,5 +27,6 @@ def web_search(state: GraphState) -> Dict[str, Any]:
         documents = [web_results]
     return {"documents": documents, "question": question}
 
+
 if __name__ == "__main__":
-    web_search(state={"question": "agent_memory", "documents": None})
+    web_search(state={"question": "agent memory", "documents": None})
